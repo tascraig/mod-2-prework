6ba7f1fa77eaf5e7317ea417e8f412f3d95dbc70
tascraig <tcraig@pursuit.org>
2024-02-23 20:58:17 -0500

    Initial Quiz App

---
+++ 0.3-quizapp/script.js
@@ -0,0 +1,48 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        answers:   [
+            { text: "Hyper Text Markup Language", correct: true },
+            { text: "High Tech Multi Layering", correct: false },
+            { text: "Hyperlinks and Text Manipulation Language", correct: false },
+            { text: "Home Tool Markup Language", correct: false },
+        ]
+    },
+    {
+        question: "Which tag is used to define the main content of an HTML document?",
+        answers:   [
+            { text: "<main>", correct: false },
+            { text: "<body>", correct: true },
+            { text: "<section>", correct: false },
+            { text: "<header>", correct: false },
+        ]
+    },
+    {
+        question: "Which of the following tags is used to create a hyperlink in HTML?",
+        answers:   [
+            { text: " <a>", correct: false },
+            { text: "<link>", correct: true },
+            { text: "<href>", correct: false },
+            { text: "<hyper>", correct: false },
+        ]
+    },
+    {
+        question: "Which tag is used to define an unordered list in HTML?",
+        answers:   [
+            { text: "<ol>", correct: false },
+            { text: "<li>", correct: false },
+            { text: "<ul>", correct: true },
+            { text: "<list>", correct: false },
+        ]
+    },
+    {
+        question: "What does the HTML attribute href specify?",
+        answers:   [
+            { text: "The color of the text", correct: false },
+            { text: "The font style", correct: false },
+            { text: "The destination of a hyperlink", correct: true },
+            { text: "The alignment of the element", correct: false },
+        ]
+    },
+
+];
